a1cdd614d5dcb3ee0f291329f0701f270b8a5b10
LemmaMoto <emanuelebuaodetti@gmail.com>
2023-10-28 14:58:01 +0200

    program well

--- robot-sim/test.py
+++ robot-sim/test.py
@@ -31,55 +31,39 @@ def turn(speed, seconds):
     R.motors[0].m0.power = 0
     R.motors[0].m1.power = 0
 
-
-def SearchToken1(code,pair):
-    if pair==1: #Search4NewToken()
-        code = None
-        dist = 100
-        for token in R.see():
-            if token.dist < dist and token.info.code not in paired_boxes:
-                dist = token.dist
-                rot_y = token.rot_y
-                code = token.info.code
+def Search4NewToken():
+    code = None
+    dist = 100
+    for token in R.see():
+        if token.dist < dist and token.info.code not in paired_boxes:
+            dist = token.dist
+            code = token.info.code
            
-        if dist == 100:
-            return -1, -1, None
-        else:
-            return dist, rot_y, code
-
-    elif pair == True: #SearchpairedToken()
-        dist = 100
-        for token in R.see():
-                if token.dist < dist and token.info.code in paired_boxes:
-                    dist = token.dist
-                    rot_y = token.rot_y
-                    code = token.info.code   
-        if dist == 100:
-            return -1, -1, None
-        else:
-            return dist, rot_y, code
-
-
-    elif pair == False: #SearchToken(code)
-        dist = 100
-        for token in R.see():
-            if token.info.code == code:
-                if token.dist < dist:
-                    dist = token.dist
-                    rot_y = token.rot_y
-                    code = token.info.code
-                    print("rot_y", rot_y)   
-        if dist == 100:
-            return -1, -1, None
-        else:
-            return dist, rot_y, code
+    if dist == 100:
+        return None
+    else:
+        return code
+    
+def SearchToken(code):
+    dist = 100
+    for token in R.see():
+      if token.info.code == code:
+        if token.dist < dist:
+            dist = token.dist
+            rot_y = token.rot_y
+            print("rot_y", rot_y)   
+    if dist == 100:
+        return -1, -1
+    else:
+        return dist, rot_y
 
 
 
-def reach_token1(code,handle, pair):
+
+def reach_token(code,handle):
     var = True
     while var:
-        dist, rot_y,code = SearchToken1(code,pair) # we look for markers
+        dist, rot_y = SearchToken(code)  # we look for markers
         if dist == -1:
             print("I don't see",code," token!!")
             turn(10, 1)  # if no markers are detected, the program ends
@@ -98,13 +82,11 @@ def reach_token1(code,handle, pair):
 
 def BringToken_i_2_1(code,code1):
     handle=False
-    pair=False
-    reach_token1(code,handle, pair) 
+    reach_token(code,handle) 
     R.grab()
     handle=True
     print("Gotcha!")
-    pair=True
-    reach_token1(code1,handle, pair)
+    reach_token(code1,handle)
     R.release()
     paired_boxes.add(code)
     print("Released!")
@@ -118,21 +100,17 @@ def main():
     while var1 :
         time=0
         if code1 == None :
-            pair=1
-            dist1, rot_y1, code1 = SearchToken1(code1,pair)
+            code1 =Search4NewToken()
             paired_boxes.add(code1)
             print("code1", code1)
             var1=False
             var2=True
             while var2:
-                code=None
-                pair=1
-                dist, rot_y, code = SearchToken1(code,pair)
+
+                code=Search4NewToken()
                 print("code", code)
                 if code != None:
                     time=0
-                    pair=True
-                    dist1, rot_y1, code1 = SearchToken1(code1,pair)
                     BringToken_i_2_1(code,code1)
                 elif code == None:
                     print("I don't see any token!!")
